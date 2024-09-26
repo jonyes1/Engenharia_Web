c7b09a82eab83b32f6fd31aa0f48724fb66abce7
jonyes1 <david.fragasva@gmail.com>
2024-09-26 19:38:28 +0100

    Adicionar arquivos de projeto.

---
+++ eng_web1/Controllers/PersonController.cs
@@ -0,0 +1,133 @@
+﻿using eng_web1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eng_web1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PersonController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Create(Person newPerson)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["values"]=newPerson.Name + " [" + newPerson.Age + "]";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        if (string.IsNullOrEmpty(collection["name"]))
+        //        {
+        //            ModelState.AddModelError("name", "Mandatory field");
+
+        //        }
+        //        if (string.IsNullOrEmpty(collection["age"]))
+        //        {
+        //            ModelState.AddModelError("age", "Mandatory field");
+        //        }
+
+        //        else
+        //        {
+        //            try
+        //            {
+        //                int aux;
+        //                aux = int.Parse(collection["age"]);
+        //                if (aux<18 || aux > 100)
+        //                {
+        //                    ModelState.AddModelError("age", "Age should be between 0 and 100 ");
+        //                }
+
+        //            }
+        //            catch (FormatException)
+        //            {
+        //                ModelState.AddModelError("age", "Must indicate a integer number ");
+        //            }
+        //        }
+        //        if (ModelState.IsValid)
+        //        {
+        //            TempData["values"] = collection["name"] + "[" + collection["age"] + "]";
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //        else
+        //        {
+        //            return View();
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ eng_web1/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace eng_web1.Models
+
+{
+    public class Person
+    {
+
+        [Required(ErrorMessage ="Mandatory field")]
+        [Display(Name="Nome do utilizador")]
+        [DataType(DataType.Password)]
+        //[StringLength(20,ErrorMessage = "jhtjghyt")]
+
+        public string? Name { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is mandatory")]
+        [Range(18, 100, ErrorMessage = "{0} must be between {1} and {2}")]
+
+        public int Age { get; set; }
+
+
+        [DataType(DataType.Date)]
+
+        public DateTime Birthday { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+    }
+}
+
